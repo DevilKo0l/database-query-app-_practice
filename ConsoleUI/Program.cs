2a6333f9a4c6e2ff62a4962cd20b17a2bad86b20
Trung Nguyen <nguyen.tcao96@gmail.com>
2020-03-27 00:44:36 +0100

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseAccess;
+using Npgsql;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NpgsqlConnection newConnect = DBConnection.GetConnection();
+
+            Console.Write("Enter the year: ");
+            string year = Console.ReadLine();
+            NpgsqlCommand cmd = new NpgsqlCommand($"SELECT title, year FROM movies WHERE year ={year}", newConnect);
+            NpgsqlDataReader dataReader = cmd.ExecuteReader();
+            Console.WriteLine($"Movie produced in {year}:");
+            while (dataReader.Read())
+            {
+                Console.WriteLine($"Title: {dataReader["title"]}\n");
+            }
+            newConnect.Close();
+        }
+    }
+}
---
+++ DatabaseAccess/DBConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace DatabaseAccess
+{
+    public class DBConnection
+    {
+        private DBConnection() { }
+
+        private static NpgsqlConnection conn;
+
+        public static NpgsqlConnection GetConnection()
+        {
+            try
+            {
+                if (conn == null)
+                {
+                    conn = new NpgsqlConnection("Server=127.0.0.1; User Id = postgres; Password = talavip123; Database = movie rental;");
+                    conn.Open();
+                }
+                Console.WriteLine("Access success");
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Access failed");
+            return conn;
+        }
+
+    }
+}
