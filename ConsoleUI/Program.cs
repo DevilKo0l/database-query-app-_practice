b4e2a3c208ed9e2c8a983a23ac67d0f06731cf20
Trung Nguyen <nguyen.tcao96@gmail.com>
2020-07-06 22:11:30 +0200

    add function for creating newclient, registering a return, display statistic

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,35 +11,70 @@ namespace ConsoleUI
             NpgsqlConnection newConnect = DBConnection.GetConnection();
             DBQuery newQuery = new DBQuery(newConnect);
 
-            bool continueProgram = true;            
+            bool continueProgram = true;
             while (continueProgram)
             {
                 DisplayMenu();
                 int userInput = int.Parse(Console.ReadLine());
+                int clientId;
+                int coppyId;
+                int movieId;
                 switch (userInput)
                 {
+
                     case 1:
-                        Console.Clear();                        
+                        Console.Clear();
                         newQuery.DisplayListMovie();
                         break;
+
                     case 2:
                         Console.Clear();
-                        Console.Write("Please enter user id: ");                        
+                        Console.Write("Please enter user id: ");
                         newQuery.DisplayUserRentalDetail(int.Parse(Console.ReadLine()));
+                        break;
 
-                        break;
                     case 3:
+                        Console.Clear();
+                        Console.Write("Enter client id: ");
+                        clientId = int.Parse(Console.ReadLine());
+                        Console.Write("Enter movie id: ");
+                        movieId = int.Parse(Console.ReadLine());
+                        newQuery.CreateNewRental(clientId, movieId);
                         break;
+
                     case 4:
+                        Console.Clear();
+                        Console.WriteLine("Enter client id: ");
+                        clientId = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter coppy id: ");
+                        coppyId = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter return date: ");
+                        string returnDate = Console.ReadLine();
+                        newQuery.RegistryReturn(clientId, coppyId, returnDate);
                         break;
+
                     case 5:
-                        break;
+                        Console.Clear();
+                        Console.Write("Enter client id: ");
+                        clientId = int.Parse(Console.ReadLine());                        
+                        newQuery.CreateNewUser(clientId);
+                        break;                        
+
                     case 6:
-                        break;
+                        Console.Clear();
+                        Console.Write("Enter movie id: ");
+                        movieId = int.Parse(Console.ReadLine());
+                        Console.Write("Enter copy id: ");
+                        coppyId = int.Parse(Console.ReadLine());
+                        newQuery.CreateNewMovie(movieId, coppyId);
+                        break;                        
+
                     case 7:
                         break;
+
                     case 8:
                         break;
+
                     default:
                         break;
                 }
@@ -57,15 +92,15 @@ namespace ConsoleUI
         }
 
         static public void DisplayMenu()
-        {            
+        {
             Console.WriteLine("---------------DVD RENTAL STORE ADMINSTRATION---------------");
             Console.WriteLine("                 1.List of avaiable movie");
             Console.WriteLine("                 2.Customer rental information");
             Console.WriteLine("                 3.Create a new rental");
-            Console.WriteLine("                 4.Registering for returning anew rental");
-            Console.WriteLine("                 5.Create a new rental");
-            Console.WriteLine("                 6.Create a new user");
-            Console.WriteLine("                 7.Number of rental");
+            Console.WriteLine("                 4.Registering for returning");
+            Console.WriteLine("                 5.Create a new user");
+            Console.WriteLine("                 6.Create a new movie");
+            Console.WriteLine("                 7.Rental statistic");
             Console.WriteLine("                 8.List of overdue rental");
             Console.Write("\nEnter your selection: ");
         }
@@ -73,7 +108,6 @@ namespace ConsoleUI
         public static bool isContinue(string ans)
         {
             return (ans.ToLower() == "y") ? true : false;
-
         }
     }
 }
--- DatabaseAccess/DBQuery.cs
+++ DatabaseAccess/DBQuery.cs
@@ -69,19 +69,143 @@ namespace DatabaseAccess
         {
             NpgsqlCommand cmd = new NpgsqlCommand($"SELECT first_name ||' '|| last_name as full_name, movies.title FROM clients " +
                                                   $"JOIN rentals ON clients.client_id = rentals.client_id " +
-                                                  $"JOIN copies ON rentals.copy_id = copies.copy_id " +
+                                                  $"JOIN copies ON rentals.copy_id = copies.copy_id " +                                      
                                                   $"JOIN movies ON copies.movie_id = movies.movie_id " +
-                                                  $"WHERE clients.client_id = {clientId};", _dbConnection);
+                                                  $"WHERE clients.client_id = @clientId;", _dbConnection);
+            cmd.Parameters.AddWithValue("@clientId", clientId);
             NpgsqlDataReader dataReader = cmd.ExecuteReader();
             dataReader.Read();
             Console.WriteLine($"{dataReader["full_name"]}'s rental history: ");
             while (dataReader.Read())
             {
-                Console.WriteLine($"{dataReader["title"]}");
+                Console.WriteLine($"{dataReader["title"]}");                
             }
             dataReader.Close();
         }
 
+        //5. Create a new rental
+        public void CreateNewRental(int clientId, int copyId)
+        {
+            string dateTime = DateTime.Now.ToString();
+            NpgsqlCommand cmd = DBConnection.GetConnection().CreateCommand();
+            cmd.Connection = DBConnection.GetConnection();
+            try
+            {
+                cmd.CommandText = $"INSERT INTO rentals(copy_id, client_id, date_of_rental) VALUES(@copyId, @clientId, '{dateTime}')";
+                cmd.Parameters.AddWithValue("@clientId", clientId);
+                cmd.Parameters.AddWithValue("@copyId", copyId);
+                cmd.Parameters.AddWithValue("@dateTime", dateTime);
+                cmd.ExecuteNonQuery();
+               
+                Console.WriteLine("record is written to database");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Error occured");               
+            };
 
+        }
+
+        //6  registering a return
+        public void RegistryReturn(int clientId, int coppyId, string returnDate)
+        {
+            string dateTime = DateTime.Now.ToString();
+            NpgsqlCommand cmd = DBConnection.GetConnection().CreateCommand();
+            NpgsqlTransaction transaction = DBConnection.GetConnection().BeginTransaction();
+            cmd.Connection = DBConnection.GetConnection();
+            cmd.Transaction = transaction;
+
+            try
+            {
+                cmd.CommandText = $"UPDATE rentals SET date_of_return = '{returnDate}' WHERE copy_id = {coppyId} AND client_id = {clientId}";
+                cmd.ExecuteNonQuery();
+                transaction.Commit();
+                Console.WriteLine("record is updated");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Error occured");
+                transaction.Rollback();
+            };
+        }
+
+        //7 Create a new user
+        public void CreateNewUser(int clientId)
+        {
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter date of birth: ");
+            string birthday = Console.ReadLine();           
+            NpgsqlCommand cmd = DBConnection.GetConnection().CreateCommand();           
+
+            try
+            {
+                cmd.CommandText = $"INSERT INTO clients(client_id, first_name, last_name, birthday) VALUES({clientId}, '{firstName}', '{lastName}','{birthday}')";
+                cmd.ExecuteNonQuery();                
+                Console.WriteLine("record is updated");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Error occured");                
+            };
+        }
+
+        public void CreateNewMovie(int movieId, int copyId)
+        {
+            Console.Write("Enter the title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Year: ");
+            string year = Console.ReadLine();
+
+            Console.Write("Enter Age restriction: ");
+            int ageRestriction = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Price: ");
+            int price = int.Parse(Console.ReadLine());
+
+            NpgsqlCommand cmd = DBConnection.GetConnection().CreateCommand();
+            NpgsqlTransaction transaction = DBConnection.GetConnection().BeginTransaction();
+            cmd.Connection = DBConnection.GetConnection();
+            cmd.Transaction = transaction;
+
+            try
+            {
+                cmd.CommandText = $"INSERT INTO movies(title, year, age_restriction, movie_id, price) VALUES('{title}', {year}, {ageRestriction}, {movieId}, {price})";
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("How many copy you want to have: ");
+                int nCopy = int.Parse(Console.ReadLine());                
+                cmd.CommandText = $"INSERT INTO copies(copy_id, available, movie_id) VALUES('{copyId}', {true}, {movieId})";
+                cmd.ExecuteNonQuery();                             
+                transaction.Commit();
+                Console.WriteLine("Both records are updated to database");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Error occured");
+                transaction.Rollback();
+            };
+            
+        }
+
+        public void ClientStatistic(int clientID)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand($"SELECT rentals.client_id, COUNT(client_id) FROM rentals " +
+                                                 $"WHERE client_id = {clientID} " +
+                                                 $"GROUP BY rentals.client_id " , _dbConnection);
+            NpgsqlDataReader dataReader = cmd.ExecuteReader();
+            Console.WriteLine($"List of actors:\n");
+
+            while (dataReader.Read())
+            {
+                Console.WriteLine($"{dataReader["first_name"]} {dataReader["last_name"]}", _dbConnection);
+            }
+        }
     }
 }
